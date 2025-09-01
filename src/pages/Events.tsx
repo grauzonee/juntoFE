65fb7c1a1a965e51d824e08f9dcb4b9b5677139f
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-09-01 10:00:56 +0200

    implement event block

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,14 +1,18 @@
 import ResponsiveComponent from "@/components/helpers/ResponsiveComponent"
+import Event from "@/components/Event"
 
 function Events() {
     return (
         <>
             <div className="w-full h-8 bg-gray-400 text-center">Search</div>
-            <div className="h-full flex flex-row w-full">
+            <div className="h-full flex flex-row w-full gap-5">
                 <ResponsiveComponent isDesktop={true}>
                     <div className="bg-rose-200 w-1/3 aspect-square">Calendar</div>
                 </ResponsiveComponent>
-                <div className="flex flex-col bg-yellow-300 text-center flex-1">Events</div>
+                <div className="flex flex-col text-center flex-1 gap-3">
+                    <Event />
+                    <Event />
+                </div>
             </div>
         </>
     )
