3bd0d94a31334d58f6d5eb98de739dd81ec258a1
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-09-01 12:13:15 +0200

    Responsive design for event block

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -9,8 +9,8 @@ import event_example from "/event_example.jpg"
 
 function Event() {
     return (
-        <Card className="p-3 backdrop-blur-md border-white/20 flex flex-row gap-3 justify-between items-center">
-            <div className="rounded-lg h-18 w-40 overflow-hidden shadow">
+        <Card className="p-3 backdrop-blur-lg shadow-lg md:shadow border-white/20 flex flex-col md:flex-row gap-3 justify-between items-center cursor-pointer">
+            <div className="rounded-lg md:max-h-18 md:w-40 w-full overflow-hidden shadow">
                 <img src={event_example} alt="Event" className="object-cover" />
             </div>
             <div className="flex flex-col flex-1 items">
@@ -18,7 +18,7 @@ function Event() {
                     <CardTitle>September challenge: 3-Hour Art Focus Inspired by book: Four Thousand Weeks</CardTitle>
                     <CardDescription>NoCrastination Club • Vienna, AT • 4.9</CardDescription>
                 </CardHeader>
-                <CardFooter className="px-3 py-1 bg-accent text-white shadow">
+                <CardFooter className="px-3 py-1 bg-accent text-white shadow font-semibold rounded-sm">
                     <p>Mon, Sep 1 · 12:50 PM GMT+2</p>
                 </CardFooter>
             </div>
