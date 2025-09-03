fbbc74823f1da06bcd12f226dace29ec4ab76bc8
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-09-03 11:12:50 +0200

    Implemented change password form using hooks

--- src/components/dialogs/EditProfileDialog.tsx
+++ src/components/dialogs/EditProfileDialog.tsx
@@ -9,8 +9,8 @@ import {
 import { MdOutlineEditNote } from "react-icons/md";
 import { Separator } from "@/components/ui/separator";
 import ResponsiveComponent from "@/components/helpers/ResponsiveComponent";
-const ChangePassword = lazy(() => import("@/components/ChangePassword"))
-import EditProfileForm from "@/components/forms/EditProfileForm"
+const ChangePasswordForm = lazy(() => import("@/components/forms/ChangePasswordForm"))
+const EditProfileForm = lazy(() => import("@/components/forms/EditProfileForm"))
 
 
 function EditProfileDialog() {
@@ -20,14 +20,14 @@ function EditProfileDialog() {
                 <MdOutlineEditNote /><span>Edit profile</span>
             </div>
         </DialogTrigger>
-        <DialogContent>
+        <DialogContent className="px-4">
             <DialogHeader>
                 <DialogTitle>Edit profile</DialogTitle>
             </DialogHeader>
-            <div className="flex flex-col md:grid gap-6 md:gap-x-12 md:grid-rows-[auto_2rem] md:grid-cols-[15rem_15rem]">
+            <div className="flex flex-col gap-3 md:flex-row justify-between">
                 <EditProfileForm />
-                <ChangePassword className="row-span-2 order-4 md:order-none" />
-                <ResponsiveComponent isMobile={true}><Separator className="my-2 order-3" /></ResponsiveComponent>
+                <ChangePasswordForm className="row-span-2 order-4 md:order-none" />
+                <ResponsiveComponent isMobile={true} isTablet={true}><Separator className="my-2 order-3" /></ResponsiveComponent>
             </div>
         </DialogContent>
     </Dialog>)
