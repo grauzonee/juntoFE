1667e3d9f926765d7588a777eb45bd0a4971d695
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-09-23 15:55:41 +0200

    Implement auth middleware

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,8 +1,9 @@
 import { lazy } from "react"
 const User = lazy(() => import("@/pages/user/User"))
 const Edit = lazy(() => import("@/pages/user/Edit"))
+import { authLoader } from "@/middlewares/AuthMiddleware"
 
 export const routes = [
-    { path: 'profile', element: <User /> },
-    { path: 'profile/edit', element: <Edit /> },
+    { path: 'profile', element: <User />, loader: authLoader },
+    { path: 'profile/edit', element: <Edit />, loadeer: authLoader },
 ]
