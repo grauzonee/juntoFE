2afb042330da0fd4387e9dd156954af369d74663
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-10-30 18:26:12 +0100

    Refactor user to subcomponents

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,16 +1,25 @@
 import EventsHistoryCard from "@/components/EventsHistoryCard"
 import MyEventsCard from "@/components/MyEventsCard"
-import UserSidebar from "@/components/UserSidebar"
+import UserCard from "@/components/cards/UserCard"
 
 function User() {
     return (
         <div className=" w-full h-full py-2">
             <div className="flex flex-col md:flex-row gap-3 mb-2 items-stretch">
-                <UserSidebar />
+                <UserCard className="flex flex-col gap-2 p-5">
+                    <UserCard.EditableImage />
+                    <div className="flex flex-row w-100 justify-between">
+                        <UserCard.Name />
+                        <UserCard.EditProfile />
+                    </div>
+                    <UserCard.Description />
+                    <UserCard.Interests />
+                    <UserCard.MemberSince />
+                </UserCard>
                 <div className="flex-1 flex flex-col gap-3">
                     <EventsHistoryCard className="h-full" />
                 </div>
-            </div>
+            </div >
             <MyEventsCard />
         </div >
     )
