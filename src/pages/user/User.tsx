dc395666c0e13f16507e9dbadea76a00d68f8f92
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-11-07 14:13:52 +0100

    Change pages after UserCard refactor

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,12 +1,15 @@
-import EventsHistoryCard from "@/components/EventsHistoryCard"
-import MyEventsCard from "@/components/MyEventsCard"
-import UserCard from "@/components/cards/UserCard"
+import EventsHistoryCard from "@/components/profile/EventsHistoryCard"
+import MyEventsCard from "@/components/profile/MyEventsCard"
+import UserCard from "@/components/profile/UserCard"
+import { UserContext } from "@/contexts/UserContext"
+import { useContext } from "react"
 
 function User() {
+    const { user } = useContext(UserContext)
     return (
         <div className=" w-full h-full py-2">
             <div className="flex flex-col md:flex-row gap-3 mb-2 items-stretch">
-                <UserCard className="flex flex-col gap-2 p-5">
+                {user && <UserCard user={user} className="flex flex-col gap-2 p-5">
                     <UserCard.EditableImage />
                     <div className="flex flex-row w-100 justify-between">
                         <UserCard.Name />
@@ -15,7 +18,7 @@ function User() {
                     <UserCard.Description />
                     <UserCard.Interests />
                     <UserCard.MemberSince />
-                </UserCard>
+                </UserCard>}
                 <div className="flex-1 flex flex-col gap-3">
                     <EventsHistoryCard className="h-full" />
                 </div>
