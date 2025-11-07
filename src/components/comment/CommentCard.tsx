421d2939a603205cd7c8ef62c1657c003b3d71ed
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-11-07 14:20:38 +0100

    Implement comment components

---
+++ src/components/comment/CommentCard.tsx
@@ -0,0 +1,29 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { type Comment } from "@/types/Comment"
+
+type CommentCardProps = {
+    comment: Comment
+}
+
+function CommentCard({ comment }: CommentCardProps) {
+    const formattedCreatedAt = new Intl.DateTimeFormat('en-US', {
+        dateStyle: 'medium',
+        timeStyle: 'short',
+    }).format(new Date(comment.createdAt));
+    return (
+        <div className="flex flex-col border-2 border-border shadow-inset p-5 bg-main/50 gap-3">
+            <div className="flex flex-row text-main-foreground gap-3 w-full items-center">
+                <Avatar className="size-12 border-2 border-background shadow-shadow">
+                    <AvatarImage src={comment.author.avatarUrl} />
+                </Avatar>
+                <div>
+                    <p className="font-semibold">{comment.author.username}</p>
+                    <small className="text-date">{formattedCreatedAt}</small>
+                </div>
+            </div>
+            <p className="text-xs">{comment.content}</p>
+        </div >
+    )
+}
+
+export default CommentCard
